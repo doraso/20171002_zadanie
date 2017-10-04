decb91866f66fbbd994dd54c7f12d1ec7bb548b5
Dorota Szwed <dorota.soroko@gmail.com>
2017-10-04 19:29:46 +0200

    Zadanie 1. Zbyt dużo spacji w println:)

--- src/ConverterTest.java
+++ src/ConverterTest.java
@@ -2,7 +2,7 @@ public class ConverterTest {
     public static void main(String[] args) {
         for (int i=1; i<=7; i++){
             String result = CalendarConverter.convertDayToString(i);
-            System.out.println(i + " " + "dzień" + " " + "tygodnia" + " " + "to" + " " + result);
+            System.out.println(i + " " + "dzień tygodnia to" + " " + result);
         }
     }
 }
